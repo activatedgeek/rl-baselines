6da7ba0b77259dd8ac672770b53965c8be497aae
Sanyam Kapoor <1sanyamkapoor@gmail.com>
2018-07-07 00:19:33 -0700

    Fix lint errors

--- torchrl/registry/problems.py
+++ torchrl/registry/problems.py
@@ -308,15 +308,18 @@ class DQNProblem(Problem):
       transition_batch = list(zip(*transition_batch))
       transition_batch = [torch.stack(item).to(self.device)
                           for item in transition_batch]
-      current_q_values, expected_q_values = self.agent.compute_q_values(*transition_batch)
-      value_loss = self.agent.learn(*transition_batch, current_q_values, expected_q_values)
+      current_q_values, expected_q_values = \
+        self.agent.compute_q_values(*transition_batch)
+      value_loss = self.agent.learn(*transition_batch,
+                                    current_q_values, expected_q_values)
       return {'value_loss': value_loss}
     return {}
 
 
 class PrioritizedDQNProblem(Problem):
   def __init__(self, hparams, problem_args, *args, **kwargs):
-    super(PrioritizedDQNProblem, self).__init__(hparams, problem_args, *args, **kwargs)
+    super(PrioritizedDQNProblem, self).__init__(
+        hparams, problem_args, *args, **kwargs)
 
     self.buffer = PrioritizedReplayBuffer(self.hparams.buffer_size)
 
@@ -331,10 +334,12 @@ class PrioritizedDQNProblem(Problem):
       transition_batch = list(zip(*transition_batch))
       transition_batch = [torch.stack(item).to(self.device)
                           for item in transition_batch]
-      current_q_values, expected_q_values = self.agent.compute_q_values(*transition_batch)
-      td_error = (current_q_values - expected_q_values).abs().detach().cpu().numpy()
-      value_loss = self.agent.learn(*transition_batch, current_q_values, expected_q_values)
-      self.buffer.update_probs(indices, td_error)
+      current_q_values, expected_q_values = \
+        self.agent.compute_q_values(*transition_batch)
+      td_error = (current_q_values - expected_q_values).abs().detach().cpu()
+      value_loss = self.agent.learn(*transition_batch,
+                                    current_q_values, expected_q_values)
+      self.buffer.update_probs(indices, td_error.numpy())
       return {'value_loss': value_loss}
     return {}
 
