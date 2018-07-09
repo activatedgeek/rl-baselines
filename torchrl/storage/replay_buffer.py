fb6bf20fa2790cc6fd2b95746ed44cb1a089f290
Sanyam Kapoor <1sanyamkapoor@gmail.com>
2018-07-08 21:43:47 -0700

    Add error weights to PER, new Schedule class

--- torchrl/storage/replay_buffer.py
+++ torchrl/storage/replay_buffer.py
@@ -1,7 +1,5 @@
 import random
 from collections import deque
-import numpy as np
-
 
 DEFAULT_BUFFER_SIZE = int(1e6)
 
@@ -29,46 +27,3 @@ class ReplayBuffer:
 
   def __len__(self):
     return len(self.buffer)
-
-
-class PrioritizedReplayBuffer(ReplayBuffer):
-  def __init__(self, size=DEFAULT_BUFFER_SIZE,
-               epsilon=0.01, alpha=1.0, beta=1.0):
-    super(PrioritizedReplayBuffer, self).__init__(size=size)
-
-    self.size = size
-    self.epsilon = epsilon
-    self.alpha = alpha
-    self.beta = beta
-    self.probs = deque(maxlen=size)
-
-  def push(self, item):
-    super(PrioritizedReplayBuffer, self).push(item)
-    self.probs.append(1.0)
-
-  def extend(self, *items):
-    super(PrioritizedReplayBuffer, self).extend(*items)
-    self.probs.extend([1.0] * len(items))
-
-  def clear(self):
-    super(PrioritizedReplayBuffer, self).clear()
-    self.probs.clear()
-
-  def sample(self, batch_size):
-    self._assert_batch_size(batch_size)
-
-    probs = np.array(self.probs)
-    probs /= np.sum(probs)
-    # weights = np.power(self.__len__() * probs, -self.beta)
-
-    indices = np.random.choice(range(self.__len__()), size=batch_size,
-                               replace=False, p=probs)
-    batch = [self.buffer[i] for i in indices]
-    return indices, batch
-
-  def update_probs(self, indices: np.array, td_error: np.array):
-    new_prob = np.power(td_error + self.epsilon, self.alpha)
-    updated_probs = np.array(self.probs)
-    updated_probs[indices] = np.squeeze(new_prob, axis=-1)
-
-    self.probs = deque(updated_probs, maxlen=self.size)
