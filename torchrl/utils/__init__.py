803f35ee8c7ab4888a2e68741a98898e7aa00560
Sanyam Kapoor <sanyam@uber.com>
2020-01-06 15:59:29 -0800

    Remove sum tree

--- torchrl/utils/__init__.py
+++ torchrl/utils/__init__.py
@@ -1,4 +1,3 @@
 from .misc import set_seeds, minibatch_generator, import_usr_dir
-from .sum_tree import SumTree
 from .schedule import LinearSchedule, ExpDecaySchedule
 from .multi_envs import MultiGymEnvs
