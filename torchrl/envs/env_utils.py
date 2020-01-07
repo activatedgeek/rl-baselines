86748e9f54029ff85b75a224dee57484c508ea0c
Sanyam Kapoor <sanyam@uber.com>
2020-01-06 18:11:53 -0800

    Transition monitor now in make_env itself

--- torchrl/envs/env_utils.py
+++ torchrl/envs/env_utils.py
@@ -1,11 +1,13 @@
 import gym
 from typing import Optional, Callable, Tuple
 
+from .wrappers import TransitionMonitor
+
 
 def make_gym_env(spec_id: str, seed: Optional[int] = None) -> gym.Env:
   env = gym.make(spec_id)
   env.seed(seed)
-  return env
+  return TransitionMonitor(env)
 
 
 def get_gym_spaces(make_env_fn: Callable[..., gym.Env]) -> Tuple[gym.Space, gym.Space]:  # pylint: disable=line-too-long
